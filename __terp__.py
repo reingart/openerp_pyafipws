9a0c680743861b3089c0573d80f142d9be9a7a91
Mariano Reingart <reingart@gmail.com>
2013-07-27 17:52:53 -0300

    added module declaration

---
+++ __terp__.py
@@ -0,0 +1,14 @@
+{
+	"name": "PyAfipWs - Argentina AFIP Webservices", # 
+	"version": "1.0",
+	"author": "Mariano Reingart / Sistemas Ágiles",
+	"description": "Autenticación, Facturación Electrónica y otros servicios",
+	"license": "Affero GPL-3",
+	"website": "http://www.sistemasagiles.com.ar/",
+	"category": "Localization/America/Argentina",
+	"depends": ["base","sale","account","crm"],
+	"init_xml": [],
+	"demo_xml": [],
+	"update_xml": [],
+	"active": False 
+}
