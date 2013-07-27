3320ad3df83315a359a5b82717d5762939ecdb0f
Mariano Reingart <reingart@gmail.com>
2013-07-27 19:08:45 -0300

    added company view for AFIP credentials (cuit, cert, pkey)

--- __terp__.py
+++ __terp__.py
@@ -3,12 +3,12 @@
 	"version": "1.0",
 	"author": "Mariano Reingart / Sistemas Ágiles",
 	"description": "Autenticación, Facturación Electrónica y otros servicios",
-	"license": "Affero GPL-3",
+	"license": "AGPL-3",
 	"website": "http://www.sistemasagiles.com.ar/",
 	"category": "Localization/America/Argentina",
 	"depends": ["base","sale","account","crm"],
 	"init_xml": [],
 	"demo_xml": [],
-	"update_xml": [],
-	"active": False 
+	"update_xml": ["data/res_company_view.xml"],
+	"active": False
 }
